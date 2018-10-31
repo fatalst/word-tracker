68d4af7f468cd0c6984183a50b0fd04d817622df
fatalst <31542223+fatalst@users.noreply.github.com>
2018-10-31 10:56:06 -0500

    parsing length, tracker

--- New.py
+++ New.py
@@ -38,6 +38,48 @@ def new():
 			print("Not a valid response. Try again.")
 			project = input(">> ")
 
+	if(project != 5):
+		print("What type of tracker do you want to use?")
+		print("1. Months.")
+		print("2. Weeks.")
+		print("3. Days.")
+		l = input(">> ")
+
+		done = True
+		while done:
+			if (project < 4 and project > 0):
+				done = False
+				length = parseLength(l)
+				# TODO: add confirmation
+				print("Your tracking type is " + length + ".\n")
+			else:
+				print("Not a valid response. Try again.")
+				project = input(">> ")
+		print("What would you like to track?")
+		print("1. Words.")
+		print("2. Scenes.")
+		print("3. Chapters")
+		print("4. Other")
+		t = input(">> ")
+
+		done = True
+		while done:
+			if (t < 5 and t > 0):
+				done = False
+				tracker = parseTracker(t)
+				# TODO: add confirmation
+				print("You will be tracking " + tracker + ".\n")
+			else:
+				print("Not a valid response. Try again.")
+				project = input(">> ")
+	else:
+		length = "days"
+		print("For NaNoWriMo, your tracking type is " + length + ".\n")
+
+		tracker = "words"
+		print("For NaNoWriMo, you will be tracking " + tracker + ".\n")
+
+
 # function to return type of project	
 def parseProject(t):
 	if (t == 1):
@@ -52,6 +94,25 @@ def parseProject(t):
 		return "nano"
 	else:
 		print("What type is your project?")
+		return raw_input(">> ")
+
+def parseLength(l):
+	if (l == 1):
+		return "months"
+	elif (l == 2):
+		return "weeks"
+	else:
+		return "days"
+
+def parseTracker(t):
+	if (t == 1):
+		return "words"
+	elif (t == 2):
+		return "scenes"
+	elif (t == 3):
+		return "chapters"
+	else:
+		print("What will you be tracking?")
 		return raw_input(">> ")
 
 def save(data):
